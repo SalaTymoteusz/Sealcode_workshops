62ec93c2a087c1713bb396d71b759ead6967463a
s426242 <32843131+s426242@users.noreply.github.com>
2017-11-27 13:57:17 +0100

    Add files via upload

---
+++ Projekt/toDo/js/date.js
@@ -0,0 +1,19 @@
+(function() {
+    function leadingZero(i) {
+        return (i < 10)? '0'+i : i;
+    }
+
+    function showTextTime() {
+        var currentDate = new Date();
+        var days = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
+        var textDate = leadingZero(currentDate.getDate()) + "." + leadingZero((currentDate.getMonth()+1)) + "." + currentDate.getFullYear() + " - " + days[currentDate.getDay()];
+
+        document.querySelector('#fData').innerHTML = textDate;
+
+        setTimeout(function() {
+            showTextTime()
+        }, 1000);
+    }
+
+    showTextTime();
+})();
